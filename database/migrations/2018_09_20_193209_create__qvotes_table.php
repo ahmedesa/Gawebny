27398cd7a57b75e8a48b7a239061145458852d91
ahmedesa <35763058+ahmedesa@users.noreply.github.com>
2018-10-21 17:20:49 +0200

    user

--- database/migrations/2018_09_20_193209_create__qvotes_table.php
+++ database/migrations/2018_09_20_193209_create__qvotes_table.php
@@ -13,7 +13,7 @@ class CreateQvotesTable extends Migration
      */
     public function up()
     {
-        Schema::create('Qvotes', function (Blueprint $table) {
+        Schema::create('qvotes', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
             $table->integer('vote');
